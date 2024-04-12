9d298c35553c307b6d575e89b7904973a128ba42
Saina Firooz <saina.firooz@gmail.com>
2024-04-12 13:40:39 +0200

    starting footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
