2987edf43d491ac2d16a3209fe0cfa39e42e561c
Saina Firooz <saina.firooz@gmail.com>
2024-04-12 13:45:53 +0200

    Using BrowserRouter for my login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-        
-    </>
-  )
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App
