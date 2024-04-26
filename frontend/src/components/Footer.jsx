a4c3da60114473a613394a720e9380a1fc4837fc
Saina Firooz <saina.firooz@gmail.com>
2024-04-26 10:15:45 +0200

    footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,121 @@
 import React from "react";
 import "../styles/Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXTwitter,
+  faThreads,
+  faFacebook,
+  faTiktok,
+  faYoutube,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 
 
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-links">
+        <div className="footer-section">
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+            <li>
+              <a href="#">Pro</a>
+            </li>
+            <li>
+              <a href="#">Apps</a>
+            </li>
+            <li>
+              <a href="#">Podcast</a>
+            </li>
+            <li>
+              <a href="#">Year in Review</a>
+            </li>
+            <li>
+              <a href="#">Gift Guide</a>
+            </li>
+            <li>
+              <a href="#">Help</a>
+            </li>
+            <li>
+              <a href="#">Terms</a>
+            </li>
+            <li>
+              <a href="#">API</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <ul className="social-icons">
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faXTwitter}
+                  style={{ color: "#999f90" }}
+                  className="soc-icon"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faThreads}
+                  style={{ color: "#999f90" }}
+                  className="soc-icon"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  style={{ color: "#999f90" }}
+                  className="soc-icon"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  style={{ color: "#999f90" }}
+                  className="soc-icon"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faTiktok}
+                  style={{ color: "#999f90" }}
+                  className="soc-icon"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  style={{ color: "#999f90" }}
+                  className="soc-icon"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer
