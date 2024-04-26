e5d03db9f7950645d669e9f6d1db6d7cf2ef3475
Saina Firooz <saina.firooz@gmail.com>
2024-04-26 10:15:57 +0200

    login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="main-content">
+      <div className="container">
+        <Header />
+        <div className="form-container">
+          <div className="card">
+            <h2>Login</h2>
+            <form>
+              <div className="form-group">
+                <label className="label" htmlFor="username">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="input"
+                />
+              </div>
+              <div className="form-group">
+                <label className="label" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="input"
+                />
+              </div>
+              <button type="submit" className="button">
+                Login
+              </button>
+            </form>
+          </div>
+          <p className="reg-p">
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default LoginPage
