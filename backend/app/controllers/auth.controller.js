021f37b576b98a6ea4714068952cf6ee1dfa1da2
Saina Firooz <saina.firooz@gmail.com>
2024-04-29 23:37:29 +0200

    hashed password using bcrypt

--- backend/app/controllers/auth.controller.js
+++ backend/app/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -19,6 +20,8 @@ export const signup = async (req, res) => {
       return res.status(400).json({ error: "Username already exists" });
     }
     // Hash password here
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     // profile pictures
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -27,19 +30,24 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
   } catch (error) {
     console.log("Error in sign up controller: ", error.message);
     res.status(500).json({ message: "Internal Server Error" });
