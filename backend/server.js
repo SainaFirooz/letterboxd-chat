730766818fcb2cf1f01212c68a8958c0feb4a911
Saina Firooz <saina.firooz@gmail.com>
2024-05-01 01:50:20 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./app/routes/auth.routes.js";
+import messageRoutes from "./app/routes/message.routes.js";
+import userRoutes from "./app/routes/user.routes.js";
+
 import connectToMongoDB from "./app/config/mongoose.js";
 
 const app = express();
@@ -9,8 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
