4266c0a98f6625a7aea0b7586ee83c4ec5a2a1eb
Saina Firooz <saina.firooz@gmail.com>
2024-05-07 20:19:42 +0200

    header component, now with tailwind/daisyui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../images/logo.png";
-import "../styles/Header.css";
+import { Link } from "react-router-dom";
+// import "../styles/Header.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={logo} alt="Letterboxd" />
+    <header className="header fixed top-0 left-0 right-0 bg-form text-white p-2 h-18 z-10">
+      <div className="logo flex items-center">
+        <Link to="/">
+          <img src={logo} alt="Letterboxd" className="w-64 h-auto ml-60" />
+        </Link>
       </div>
     </header>
   );
