6a92ab61c086948eb76ff6aed6099990a5b4d6b3
Saina Firooz <saina.firooz@gmail.com>
2024-05-07 20:19:25 +0200

    footer component, now with tailwind/daisyui

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,121 +1,94 @@
 import React from "react";
-import "../styles/Footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faXTwitter,
-  faThreads,
-  faFacebook,
-  faTiktok,
-  faYoutube,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+import logo from "../images/dots_logo.png";
 
-
-
-
+// import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-links">
-        <div className="footer-section">
-          <ul>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">News</a>
-            </li>
-            <li>
-              <a href="#">Pro</a>
-            </li>
-            <li>
-              <a href="#">Apps</a>
-            </li>
-            <li>
-              <a href="#">Podcast</a>
-            </li>
-            <li>
-              <a href="#">Year in Review</a>
-            </li>
-            <li>
-              <a href="#">Gift Guide</a>
-            </li>
-            <li>
-              <a href="#">Help</a>
-            </li>
-            <li>
-              <a href="#">Terms</a>
-            </li>
-            <li>
-              <a href="#">API</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
+    <footer className="footer p-0 bg-neutral text-neutral-content w-full">
+      <aside className="p-2">
+        <img
+          src={logo}
+          alt="letterboxd logo"
+          width="100"
+          height="100"
+          className="fill-current ml-8 "
+        />
+        <p className="footer-title text-xs ml-4  ">
+          © Letterboxd Limited.
+          <br />
+          Made by Saina
+        </p>
+      </aside>
+      <nav className="flex justify-end gap-8 p-5 footer-title">
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          About
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          News
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          Pro
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          Apps
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          Podcast
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          Year in Review
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          Help
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          API
+        </a>
+        <a className="link link-hover  hover:text-hoverColor hover:no-underline">
+          Contact
+        </a>
+      </nav>
+      <nav>
+        {/* <h6 className="footer-title">Social</h6> */}
+        <div className="grid grid-flow-col gap-4 right-0 p-5">
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current  hover:text-hoverColor hover:no-underline"
+            >
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current  hover:text-hoverColor hover:no-underline"
+            >
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current  hover:text-hoverColor hover:no-underline"
+            >
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
         </div>
-        <div className="footer-section">
-          <ul className="social-icons">
-            <li>
-              <a href="#">
-                <FontAwesomeIcon
-                  icon={faXTwitter}
-                  style={{ color: "#999f90" }}
-                  className="soc-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FontAwesomeIcon
-                  icon={faThreads}
-                  style={{ color: "#999f90" }}
-                  className="soc-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FontAwesomeIcon
-                  icon={faFacebook}
-                  style={{ color: "#999f90" }}
-                  className="soc-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FontAwesomeIcon
-                  icon={faInstagram}
-                  style={{ color: "#999f90" }}
-                  className="soc-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FontAwesomeIcon
-                  icon={faTiktok}
-                  style={{ color: "#999f90" }}
-                  className="soc-icon"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FontAwesomeIcon
-                  icon={faYoutube}
-                  style={{ color: "#999f90" }}
-                  className="soc-icon"
-                />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      </nav>
     </footer>
   );
 };
 
-export default Footer
+export default Footer;
