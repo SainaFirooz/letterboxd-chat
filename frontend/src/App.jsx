24e523d89410ff653be8d8c6187518addfe8c7ba
Saina Firooz <saina.firooz@gmail.com>
2024-05-08 23:46:56 +0200

    Update App.jsx to use react-router-dom and AuthContextProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ChatPage from "./pages/ChatPage";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-
+  const { authUser } = useAuthContext();
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
-        </Routes>
-      </Router>
+      {/* <Router> */}
+      {/* <div className="p-4 h-screen items-center justify-center"> */}
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Navigate to="/home" /> : <LoginPage />}
+        ></Route>
+        <Route
+          path="/register"
+          element={authUser ? <Navigate to="/home" /> : <RegisterPage />}
+        ></Route>
+        <Route
+          path="/home"
+          element={authUser ? <ChatPage /> : <Navigate to="/" />}
+        ></Route>
+      </Routes>
+      {/* </div> */}
+      {/* </Router> */}
+      <Toaster position="bottom-right" reverseOrder={false} />
     </div>
   );
 }
 
-export default App
+export default App;
