b7d5abc5055a15c028691c967aea02e354bce943
Saina Firooz <saina.firooz@gmail.com>
2024-05-08 23:50:46 +0200

    updated messages component, added useRef, useEffect with setTimeout

--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../Skeleton/MessageSkeleton";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1 overflow-y-scroll no-scrollbar p-20 py-2 flex flex-col justify-between">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton ket={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-footerLink">
+          Send a message to start the conversation
+        </p>
+      )}
     </div>
   );
 };
