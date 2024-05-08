d4314cbecbbc1c7464884fc734dc5d487a124918
Saina Firooz <saina.firooz@gmail.com>
2024-05-08 23:44:01 +0200

    Add useAuthContext hook for accessing authentication context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,11 @@
 import { createContext, useState } from "react";
+import React, { useContext } from "react";
 
 export const AuthContext = createContext();
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
 
 export const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(
