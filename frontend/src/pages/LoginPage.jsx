614f9d765bb71078ab67733a403f99ad7780c7c0
Saina Firooz <saina.firooz@gmail.com>
2024-05-08 23:42:22 +0200

    Add login functionality and form submission in LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import useLogin from "../hooks/useLogin";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
@@ -13,16 +24,18 @@ const LoginPage = () => {
             <div className="form-container max-w-sm mx-auto my-20">
               <div className="card bg-form text-white shadow-md rounded-lg p-5 mb-5">
                 <h2 className="text-2xl mb-5">Login</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="form-group mb-5">
                     <label className="label block mb-1" htmlFor="username">
                       Username
                     </label>
                     <input
                       type="text"
-                      id="username"
-                      name="username"
-                      className="input w-11/12 px-2.5 py-2.5 border border-gray-300 rounded-lg"
+                      // id="username"
+                      // name="username"
+                      className="input w-11/12 px-2.5 py-2.5 border border-gray-300 rounded-lg text-letterboxd"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
                     />
                   </div>
                   <div className="form-group mb-5">
@@ -31,16 +44,23 @@ const LoginPage = () => {
                     </label>
                     <input
                       type="password"
-                      id="password"
-                      name="password"
-                      className="input w-11/12 px-2.5 py-2.5 border border-gray-300 rounded-lg"
+                      // id="password"
+                      // name="password"
+                      className="input w-11/12 px-2.5 py-2.5 border border-gray-300 rounded-lg text-letterboxd"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
                   <button
                     type="submit"
                     className="button bg-green-600 text-white px-5 py-2.5 border-none cursor-pointer text-lg uppercase rounded-lg font-bold hover:bg-green-500 btn-block"
+                    disabled={loading}
                   >
-                    Login
+                    {loading ? (
+                      <span className="loading loading-spinner"></span>
+                    ) : (
+                      "Login"
+                    )}
                   </button>
                 </form>
               </div>
