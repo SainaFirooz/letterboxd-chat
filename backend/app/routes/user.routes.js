ce3caba1345756bb4f0ed334e89829ae8e881eab
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:04:54 +0200

    added search and block routes for user

--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { getUsersForSidebar } from "../controllers/user.controller.js";
+import {
+  getUsersForSidebar,
+  searchUsers,
+  blockUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getUsersForSidebar);
+router.post("/search", protectRoute, searchUsers);
+router.post("/block", protectRoute, blockUser);
 
 export default router;
