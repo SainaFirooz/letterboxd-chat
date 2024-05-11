d0181f5149ee86ac48e520d2ca637a765810ce2f
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:02:52 +0200

    added socked io functionality to message controller

--- backend/app/controllers/message.controller.js
+++ backend/app/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
+import { io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -30,12 +32,16 @@ export const sendMessage = async (req, res) => {
       // res.status(200).json({ message: "Message sent successfully" });
     }
 
-    // SOCKET IO FUNCTIONALITY WILL GO HERE
-
     // await conversation.save();
     // await newMessage.save();
 
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
