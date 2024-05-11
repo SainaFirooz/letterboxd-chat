c327f58fa03760adf4c390e28f094a9ea466f566
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:02:32 +0200

    added endpoint for conversation

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./app/routes/auth.routes.js";
 import messageRoutes from "./app/routes/message.routes.js";
 import userRoutes from "./app/routes/user.routes.js";
+import conversationRoutes from "./app/routes/conversation.routes.js";
 
 import connectToMongoDB from "./app/config/mongoose.js";
+import { app, server } from "./app/socket/socket.js";
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -18,8 +20,9 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/conversations", conversationRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
