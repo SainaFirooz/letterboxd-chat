cc9e94e2098c83c7fdc092730f8aebc220a3ab1f
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:19:18 +0200

    Update ChatPage component structure and remove unused code

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -12,16 +12,35 @@ const ChatPage = () => {
         <div className="main-content py-5">
           <Header />
 
-          <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-clip-padding backgrop-filter backdrop-blur-lg bg-opacity-0">
+          {/* flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 */}
+
+          <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-clip-padding   ">
             <Sidebar />
             <MessageContainer />
           </div>
         </div>
 
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
   );
 };
 
 export default ChatPage;
+
+// const ChatPage = () => {
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       <Header />
+//       <div className="flex-grow">
+//         <div className="main-content py-5">
+//           <div className="flex rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+//             <Sidebar />
+//             <MessageContainer />
+//           </div>
+//         </div>
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// };
