0c3f7a1c57ec51d0175a8c6e39ddac9215a9211e
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:14:04 +0200

    Sort conversations based on online status
    - Add sorting function to sort conversations based on online status of users.
    - Display a "New Message!" notification for conversations with new messages.

--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Conversation from "./Conversation";
 import useGetConversations from "../../hooks/useGetConversations";
+import { useSocketContext } from "../../context/SocketContext";
+import useSearchUsers from "../../hooks/useSearchUsers";
+import useConversation from "../../zustand/useConversation";
 
 const Conversations = () => {
   const { loading, conversations } = useGetConversations();
+  const { onlineUsers } = useSocketContext();
+  const { notifications } = useConversation();
+
+  const sortConversations = (conversations) => {
+    return [...conversations].sort((a, b) => {
+      const aIsOnline = onlineUsers.includes(a._id);
+      const bIsOnline = onlineUsers.includes(b._id);
+
+      if (aIsOnline && !bIsOnline) {
+        return -1;
+      }
+
+      if (bIsOnline && !aIsOnline) {
+        return 1;
+      }
+
+      return 0;
+    });
+  };
 
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      {conversations.map((conversation, idx) => (
-        <Conversation
-          key={conversation._id}
-          conversation={conversation}
-          lastIdx={idx === conversations.length - 1}
-        />
+      {sortConversations(conversations).map((conversation, idx) => (
+        <div key={conversation._id}>
+          <Conversation
+            conversation={conversation}
+            lastIdx={idx === conversations.length - 1}
+          />
+          {notifications.includes(conversation) && (
+            <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+              New Message!
+            </span>
+          )}
+        </div>
       ))}
 
       {loading ? (
