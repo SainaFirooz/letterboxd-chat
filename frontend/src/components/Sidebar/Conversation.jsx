5b5bf25c381b0446e4f445bab77b922f5fba1035
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:13:25 +0200

    Updated Conversation component
    - Add shake animation to new message bubbles when in the same conversation.
    - Scroll into view selected conversation using `scrollIntoView` method.
    - Integrate with socket context for online status of users.

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import useConversation from "../../zustand/useConversation";
+import { useSocketContext } from "../../context/SocketContext";
 
 const Conversation = ({ conversation, lastIdx }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+  const conversationRef = useRef(null);
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+  const shakeClass =
+    selectedConversation && selectedConversation.shouldShake ? "shake" : "";
+
+  const { newMessage } = useConversation();
+
+  useEffect(() => {
+    if (isSelected && conversationRef.current) {
+      conversationRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [isSelected]);
+
   return (
     <>
       <div
-        className={`flex gap-2 items-center hover:bg-green-500 rounded p-2 py-1 cursor-pointer ${
+        ref={conversationRef}
+        className={`flex gap-2 items-center hover:bg-green-500 rounded p-2 py-1 cursor-pointer ${shakeClass} ${
           isSelected ? "bg-green-500" : ""
         }`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
@@ -21,7 +37,7 @@ const Conversation = ({ conversation, lastIdx }) => {
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">{conversation.firstName}</p>
+            <p className="font-bold text-gray-200">@{conversation.username}</p>
           </div>
         </div>
       </div>
