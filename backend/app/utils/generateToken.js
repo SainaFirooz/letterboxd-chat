b85e9d1356078f25d3892f1bd8f8264e6d8a19c8
Saina Firooz <saina.firooz@gmail.com>
2024-05-12 01:05:11 +0200

    changed jwt token expiry date to 2h

--- backend/app/utils/generateToken.js
+++ backend/app/utils/generateToken.js
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "15d",
+    expiresIn: "2h",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
+    maxAge: 2 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
     secure: process.env.NODE_ENV === "development",
